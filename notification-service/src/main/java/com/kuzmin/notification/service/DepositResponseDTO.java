a5a51566c052a9fe3c8d1ab08dd9f2c5dccd24b5
Kirill Kuzmin <kirillkuzminr@gmail.com>
2023-08-05 23:05:51 +0300

    Implemented a service for send notification.

---
+++ notification-service/src/main/java/com/kuzmin/notification/service/DepositResponseDTO.java
@@ -0,0 +1,15 @@
+package com.kuzmin.notification.service;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class DepositResponseDTO {
+
+    private BigDecimal amount;
+
+    private String mail;
+}
