a00aea21987025509dd0adf1cffd73fb3f625f9a
Kirill Kuzmin <kirillkuzminr@gmail.com>
2023-08-05 21:10:14 +0300

    Implementation of the account management service

---
+++ account-service/src/main/java/com/kuzmin/account/controller/AccountController.java
@@ -0,0 +1,46 @@
+package com.kuzmin.account.controller;
+
+import com.kuzmin.account.controller.dto.AccountRequestDTO;
+import com.kuzmin.account.controller.dto.AccountResponseDTO;
+import com.kuzmin.account.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/{accountId}")
+    public AccountResponseDTO getAccount(@PathVariable Long accountId) {
+        return new AccountResponseDTO(accountService.getAccountById(accountId));
+    }
+
+    @PostMapping("/")
+    public Long createAccount(@RequestBody AccountRequestDTO accountRequestDTO) {
+        return accountService.createAccount(accountRequestDTO.getName(),
+                accountRequestDTO.getEmail(),
+                accountRequestDTO.getPhone(),
+                accountRequestDTO.getBills());
+    }
+
+    @PutMapping("/{accountId}")
+    public AccountResponseDTO updateAccount(@PathVariable Long accountId,
+                                            @RequestBody AccountRequestDTO accountRequestDTO) {
+        return new AccountResponseDTO(accountService.updateAccount(accountId,
+                accountRequestDTO.getName(),
+                accountRequestDTO.getEmail(),
+                accountRequestDTO.getPhone(),
+                accountRequestDTO.getBills()));
+    }
+
+    @DeleteMapping("/")
+    public AccountResponseDTO deleteAccount(@PathVariable Long accountId) {
+        return new AccountResponseDTO(accountService.deleteAccount(accountId));
+    }
+}
